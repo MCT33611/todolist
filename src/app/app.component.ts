178486d984e37e59106fb82355ef25d1808a04b4
MCT33611 <muhammedfahmi04@gmail.com>
2024-02-01 19:55:40 +0530

    final totch finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchAndCreateComponent } from './search-and-create/search-and-create.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SearchAndCreateComponent,TaskListComponent],
+  imports: [RouterOutlet,SearchAndCreateComponent,TaskListComponent,DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todolist';
   Task = ""
+  Today = new Date()
   updateEvent(key:string){
     this.Task = key;
   }
